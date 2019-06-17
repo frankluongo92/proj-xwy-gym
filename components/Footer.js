adeea49938cedfb3b30837c1a534dba52f7930d6
Frank Luongo <frank.luongo@revzilla.com>
2019-06-17 11:58:33 -0400

    Finishing touches

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import Logo from '../svgs/Logo';
+import Navigation from './Navigation';
+
 const Footer = () => {
+  const Year = () => {
+    return new Date().getFullYear();
+  }
+
   return (
-    <footer>
-      this is the footer
+
+    <footer className="footer">
+      <div className="grid-container">
+        <section className="footer__logo">
+            <Logo />
+        </section>
+        <section className="footer__links">
+          <Navigation
+            customClassName="footer-navigation"
+          />
+        </section>
+        <section className="footer__copyright footer-copyright">
+            © <Year /> XWY Gym All Rights Reserved
+        </section>
+      </div>
     </footer>
   )
 }
--- components/Tile.js
+++ components/Tile.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Waypoint } from 'react-waypoint';
+import { animated, useSpring } from 'react-spring';
 
 const Tile = ({ color, children}) => {
+  const [active, toggleActive] = useState(false);
+
+  const animation = useSpring({
+    opacity: active ? 1 : 0,
+    transform: active ? 'scale(1)' : 'scale(0)'
+  });
+
+  const showTile = () => {
+    toggleActive(true);
+  }
+
+  const hideTile = () => {
+    toggleActive(false);
+  }
 
   return (
-    <li className={`tile tile--${color}`}>
+    <Waypoint onEnter={showTile} onLeave={hideTile}>
+    <animated.li className={`tile tile--${color}`} style={animation}>
       <div className="tile__content">
         { children }
       </div>
-    </li>
+    </animated.li>
+    </Waypoint>
   )
 }
 
